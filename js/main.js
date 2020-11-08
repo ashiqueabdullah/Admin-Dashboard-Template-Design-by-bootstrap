5137b6cc4b7cc4cacafe39cba6f2819af6ab5628
Ashique Abdullah <ashiqueashique465@gmail.com>
2020-11-08 19:02:32 +0600

    Complete with fully responsive

--- js/main.js
+++ js/main.js
@@ -1,4 +1,21 @@
 $(document).ready(function () {
 
+	var allclick =  document.querySelectorAll('.clickdorp');
+	var allclicks = document.querySelectorAll('.mydeopdown');
+
+	for(let i=0; i < allclick.length; i++){
+		allclick[i].addEventListener('click',function(){
+			for(let j=0; j<allclicks.length; j++ ){
+				if(allclicks[j].style.display=='block' && i!=j){
+					allclicks[j].style.display='none';
+				}
+			}
+			if(allclicks[i].style.display=='block'){
+				allclicks[i].style.display='none';
+			}else{
+				allclicks[i].style.display='block';
+			}
+		})
+	}
 
 });
